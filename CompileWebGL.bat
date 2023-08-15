31ae04ae1bcfd3a56b85ebedfb79c512f3cc8d67
Jose Rankis <141494710+joserkis@users.noreply.github.com>
2023-08-15 18:48:52 +0300

    bats added

---
+++ CompileWebGL.bat
@@ -0,0 +1,1 @@
+ "c:\Program Files\Unity Installs\2019.3.11f1\Editor\Unity.exe" -batchmode -logFile stdout.log -executeMethod Builder.BuildWebGL -quit 
---
+++ CompileWindows.bat
@@ -0,0 +1,1 @@
+ "c:\Program Files\Unity Installs\2019.3.11f1\Editor\Unity.exe" -batchmode -buildWindowsPlayer "BuildWindows/Break.exe" -nographics -quit 
